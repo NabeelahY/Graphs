289469e563a214f46f046407484d0b9c5b5a48f7
Nabeelah <noblenab13@gmail.com>
2019-11-05 16:51:02 +0100

    Convert list of tuples to graph

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,13 @@
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    vertices = {}
+    for i in ancestors:
+        vertices[i[0]] = set() 
+    for i in ancestors:
+        if i[0] in vertices:
+            vertices[i[0]].add(i[1]) 
+
+    
+
+print(earliest_ancestor(test_ancestors, 1))
